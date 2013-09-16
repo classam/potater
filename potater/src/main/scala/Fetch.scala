39012544c6f3b7652a03f60b103e42f9ce8f6d02
Curtis Lassam <curtis@lassam.net>
2013-09-16 16:12:10 -0700

    Fetch now has 10 seconds to work with

--- potater/src/main/scala/Fetch.scala
+++ potater/src/main/scala/Fetch.scala
@@ -3,6 +3,7 @@ package net.lassam
 import scala.xml._
 import java.net.MalformedURLException
 import java.net.URL
+import java.net.URLConnection
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.io.IOException
@@ -10,8 +11,11 @@ import scala.collection.mutable.StringBuilder
 
 class Fetch(val url:String) {
   /* Throws MalformedURLException and IOException */
-  private val url_object = new URL(url)
-  private val reader = new BufferedReader(new InputStreamReader(url_object.openStream()))
+  private val urlObject = new URL(url)
+  private val urlConnection = urlObject.openConnection();
+  urlConnection.setConnectTimeout(5000)
+  urlConnection.setReadTimeout(5000)
+  private val reader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))
   private var builder = new StringBuilder()
   private var line = reader.readLine()
   while( line != null ){
