3490ea3dc5a9635b7de177d48d9b39a3f8ad4a2a
Curtis Lassam <curtis@lassam.net>
2013-09-16 15:02:44 -0700

    Separated intents to avoid the overflow error that scala throws when a function's pattern match is too beeeeeg

--- potater/src/main/scala/App.scala
+++ potater/src/main/scala/App.scala
@@ -20,108 +20,16 @@ class App extends unfiltered.filter.Plan {
   val JSON = new ResponseHeader("Content-Type", "application/json" :: Nil)
   val datastore:DatastoreService = DatastoreServiceFactory.getDatastoreService();
 
-  def intent = {
+  def intent = homeIntent.onPass(userIntent.onPass(processingIntent.onPass(subscriptionIntent.onPass(feedIntent.onPass(teapotIntent)))))
+
+  def homeIntent = unfiltered.filter.Intent {
     case GET(Path("/")) => {
       Html(<h1>Hello, World</h1>)
     }
-    // GET /users : log me in, or redirect me to /users/classam
-    case req @ GET(Path("/users")) => {
-      Auth.enticate(req)
-    }
-    // GET /users/classam : tell me if classam exists
-    case GET(Path(Seg("users" :: username :: Nil) ) ) if Auth.check(username) => {
-      val user = User.get(username, datastore)
-      if( user.isDefined ){
-        JSON ~> ResponseString( user.get.entity.getKey.toString )
-      }
-      else{
-        NotFound ~> ResponseString( "User doesn't exist" )
-      }
-    }
-    // POST /users/classam : create user classam
-    case PUT(Path(Seg("users" :: username :: Nil) ) ) if Auth.check(username) => {
-      var user = User.get(username, datastore)
-      if( !user.isDefined ){
-        user = Option.apply(User.create(username, datastore))
-      }
-      JSON ~> ResponseString( user.get.entity.getKey.toString )
-    }
-    // GET /users/classam/subscriptions : display all of my subscriptions
-    case GET(Path(Seg("users" :: username :: "subscriptions" :: Nil))) if Auth.check(username) => {
-      JSON ~> ResponseString( HasJsonObject.listJson(Subscription.getSubscriptionsForUser(username, datastore)))
-    }
-    // GET /users/classam/subscriptions/http://curtis.lassam.net/feed.xml : show this subscription 
-    case GET(Path(Seg("users" :: username :: "subscriptions" :: tail))) if Auth.check(username) => {
-      val url = tail.mkString("/")
-      var subscription = Subscription.get(username, url, datastore)
-      if(!subscription.isDefined) {
-        NotFound ~> ResponseString( "That subscription doesn't exist." )
-      }
-      JSON ~> ResponseString( subscription.get.json )
-    }
-    // POST /users/classam/subscriptions/http://curtis.lassam.net/feed.xml : create this subscription
-    //    this will also queue up this subscription's Feed for processing. 
-    case POST(Path(Seg("users" :: username :: "subscriptions" :: tail))) if Auth.check(username) => {
-      val url = tail.mkString("/")
-      var subscription = Subscription.get(username, url, datastore)
-      if( !subscription.isDefined ){ 
-        
-        // Check if the Feed exists.
-        var feed = Feed.get(url, datastore)
-        if(!feed.isDefined) {
-          feed = Option.apply( Feed.create(url, datastore) )
-        }
-
-        subscription = Option.apply( Subscription.create( username, url, datastore )) 
-
-        // Now add the subscription to the processing queue. 
-        val queue:Queue = QueueFactory.getDefaultQueue();
-        queue.add(withUrl("/process/"+url))
-      }
-      JSON ~> ResponseString( subscription.get.json )
-    }
-    // TODO: POST /users/classam/subscriptions/http://curtis.lassam.net/feed.xml : change the Category of this subscription
-    // GET /users/classam/articles : show top 100 unread ArticleStubs for classam
-    case GET(Path(Seg("users" :: username :: "articles" :: Nil))) if Auth.check(username) => {
-      JSON ~> ResponseString( HasJsonObject.listJson(ArticleStub.getArticleStubsForUser(username, 100, datastore)))
-    }
-    // GET /users/classam/articles/http://curtis.lassam.net/feed.xml : show top unread ArticleStubs for classam in this feed.
-    case GET(Path(Seg("users" :: username :: "articles" :: tail))) if Auth.check(username) => {
-      val url = tail.mkString("/")
-      val json = HasJsonObject.listJson( ArticleStub.getArticleStubsForSubscription( Subscription.generateKey(username, url), 1000, datastore ))
-      JSON ~> ResponseString( json )
-    }
-    // TODO: GET /users/classam/articles/1238382932/http://curtis.lassam.net/feed.xml : show article details  
-    // TODO: POST /users/classam/articles/1238382932/http://curtis.lassam.net/feed.xml : set article to "read" 
-    // TODO: GET /users/classam/articles/bycategory/tech : Get ArticleStubs by subscription category
-
-    // GET /users/classam/* : bounce unauthenticated users. 
-    case GET(Path(Seg("users" :: username :: tail))) => {
-      Unauthorized ~> ResponseString( "You are not that person!" )
-    }
-    // GET /articles/2983883293/http://curtis.lassam.net/feed.xml : show the actual article for feed at GUID.
-    case GET(Path(Seg("articles" :: guid :: tail))) => {
-      val url = tail.mkString("/")
-      val article = Article.get( url, guid, datastore )
-      if (!article.isDefined){
-        NotFound ~> ResponseString( "That article doesn't exist." )
-      }
-      JSON ~> ResponseString( article.get.json )
-    }
-    // GET /feeds/http://curtis.lassam.net/feed.xml : display feed details 
-    case GET(Path(Seg("feeds" :: tail))) => {
-      val url = tail.mkString("/")
-      val feed = Feed.get(url, datastore)
-      if( !feed.isDefined ){
-        NotFound ~> ResponseString( "That feed doesn't exist." )
-      }
-      JSON ~> ResponseString( feed.get.json )
-    }
-    // GET /teapot : I am a teapot
-    case GET(Path(Seg("teapot" :: Nil))) => {
-      TeaPot ~> ResponseString( "I am a teapot." )
-    }
-    // TODO: /process : load all feeds into Queue
+  }
+  
+  def processingIntent = unfiltered.filter.Intent {
+    // TODO: POST /process : load all feeds into Queue
     // GET /process/http://curtis.lassam.net/feed.xml : check this feed.
     case POST(Path(Seg("process" :: tail))) => {
       val url = tail.mkString("/")
@@ -172,6 +80,117 @@ class App extends unfiltered.filter.Plan {
       }
       
       JSON ~> ResponseString( feed.json )
+    }
+
+  }
+
+  def subscriptionIntent = unfiltered.filter.Intent {
+    // GET /users/classam/subscriptions : display all of my subscriptions
+    case GET(Path(Seg("users" :: username :: "subscriptions" :: Nil))) if Auth.check(username) => {
+      JSON ~> ResponseString( HasJsonObject.listJson(Subscription.getSubscriptionsForUser(username, datastore)))
+    }
+    // GET /users/classam/subscriptions/http://curtis.lassam.net/feed.xml : show this subscription 
+    case GET(Path(Seg("users" :: username :: "subscriptions" :: tail))) if Auth.check(username) => {
+      val url = tail.mkString("/")
+      var subscription = Subscription.get(username, url, datastore)
+      if(!subscription.isDefined) {
+        NotFound ~> ResponseString( "That subscription doesn't exist." )
+      }
+      JSON ~> ResponseString( subscription.get.json )
+    }
+    // POST /users/classam/subscriptions/http://curtis.lassam.net/feed.xml : create this subscription
+    //    this will also queue up this subscription's Feed for processing. 
+    case POST(Path(Seg("users" :: username :: "subscriptions" :: tail))) if Auth.check(username) => {
+      val url = tail.mkString("/")
+      var subscription = Subscription.get(username, url, datastore)
+      if( !subscription.isDefined ){ 
+        
+        // Check if the Feed exists.
+        var feed = Feed.get(url, datastore)
+        if(!feed.isDefined) {
+          feed = Option.apply( Feed.create(url, datastore) )
+        }
+
+        subscription = Option.apply( Subscription.create( username, url, datastore )) 
+
+        // Now add the subscription to the processing queue. 
+        val queue:Queue = QueueFactory.getDefaultQueue();
+        queue.add(withUrl("/process/"+url))
+      }
+      JSON ~> ResponseString( subscription.get.json )
+    }
+
+  }
+
+  def userIntent = unfiltered.filter.Intent{
+    // GET /users : log me in, or redirect me to /users/classam
+    case req @ GET(Path("/users")) => {
+      Auth.enticate(req)
+    }
+    // GET /users/classam : tell me if classam exists
+    case GET(Path(Seg("users" :: username :: Nil) ) ) if Auth.check(username) => {
+      val user = User.get(username, datastore)
+      if( user.isDefined ){
+        JSON ~> ResponseString( user.get.entity.getKey.toString )
+      }
+      else{
+        NotFound ~> ResponseString( "User doesn't exist" )
+      }
+    }
+    // POST /users/classam : create user classam
+    case PUT(Path(Seg("users" :: username :: Nil) ) ) if Auth.check(username) => {
+      var user = User.get(username, datastore)
+      if( !user.isDefined ){
+        user = Option.apply(User.create(username, datastore))
+      }
+      JSON ~> ResponseString( user.get.entity.getKey.toString )
+    }
+    // TODO: POST /users/classam/subscriptions/http://curtis.lassam.net/feed.xml : change the Category of this subscription
+    // GET /users/classam/articles : show top 100 unread ArticleStubs for classam
+    case GET(Path(Seg("users" :: username :: "articles" :: Nil))) if Auth.check(username) => {
+      JSON ~> ResponseString( HasJsonObject.listJson(ArticleStub.getArticleStubsForUser(username, 100, datastore)))
+    }
+    // GET /users/classam/articles/http://curtis.lassam.net/feed.xml : show top unread ArticleStubs for classam in this feed.
+    case GET(Path(Seg("users" :: username :: "articles" :: tail))) if Auth.check(username) => {
+      val url = tail.mkString("/")
+      val json = HasJsonObject.listJson( ArticleStub.getArticleStubsForSubscription( Subscription.generateKey(username, url), 1000, datastore ))
+      JSON ~> ResponseString( json )
+    }
+    // TODO: GET /users/classam/articles/1238382932/http://curtis.lassam.net/feed.xml : show article details  
+    // TODO: POST /users/classam/articles/1238382932/http://curtis.lassam.net/feed.xml : set article to "read" 
+    // TODO: GET /users/classam/articles/bycategory/tech : Get ArticleStubs by subscription category
+
+    // GET /users/classam/* : bounce unauthenticated users. 
+    case GET(Path(Seg("users" :: username :: tail))) => {
+      Unauthorized ~> ResponseString( "You are not that person!" )
+    }
+    // GET /articles/2983883293/http://curtis.lassam.net/feed.xml : show the actual article for feed at GUID.
+    case GET(Path(Seg("articles" :: guid :: tail))) => {
+      val url = tail.mkString("/")
+      val article = Article.get( url, guid, datastore )
+      if (!article.isDefined){
+        NotFound ~> ResponseString( "That article doesn't exist." )
+      }
+      JSON ~> ResponseString( article.get.json )
+    }
+  }
+
+  def feedIntent = unfiltered.filter.Intent{
+    // GET /feeds/http://curtis.lassam.net/feed.xml : display feed details 
+    case GET(Path(Seg("feeds" :: tail))) => {
+      val url = tail.mkString("/")
+      val feed = Feed.get(url, datastore)
+      if( !feed.isDefined ){
+        NotFound ~> ResponseString( "That feed doesn't exist." )
+      }
+      JSON ~> ResponseString( feed.get.json )
+    }
+  }
+
+  def teapotIntent = unfiltered.filter.Intent{
+    // GET /teapot : I am a teapot
+    case GET(Path(Seg("teapot" :: Nil))) => {
+      TeaPot ~> ResponseString( "I am a teapot." )
     }
   }
 }
